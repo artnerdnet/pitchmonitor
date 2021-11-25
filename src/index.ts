bd21801ab5094f85c8368c75bbd7306adc36d6b8
Violeta <artnerdnet@gmail.com>
2021-11-25 15:12:49 +0100

    fix for imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-export { getScalesWithinRange, fixNumberDecimals, getAllFrequencies, isNumberInBetween, findDeviation, findClosestNumber, findPreviousAndNextNote } from './helpers/utils';
-export { NOTES, referenceNote, MAGNITUDE_THRESHOLD, acceptableDeviationFromPitch } from './helpers/constants';
-export { calculateAmplitudes } from './audioProcessing/calculateAmplitudes';
-export { interpretCorrelations, findDominantFrequency } from './audioProcessing/interpretCorrelation';
-export { computeMagnitudes, calculateAverageMagnitudeValues, findMaximumMagnitude } from './audioProcessing/calculateMagnitudes';
-export { calcNotesFreq, calcEqualTemperamentFreq, findClosestPitch } from './frequencyComputations/frequencyComputations';
+export * as frequencyComputations from './frequencyComputations';
+export * as audioProcessing from './audioProcessing'
+export * as helpers from './helpers'
+
